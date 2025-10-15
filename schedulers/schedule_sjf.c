e7f89639aa4414aaf3b52908b3ab2391358ec2d3
Caleb Oglesby <noobyman68@gmail.com>
2025-10-15 16:30:03 -0400

    implemented schedule_sjf.c with sort at end instead of at list creation

--- schedulers/schedule_sjf.c
+++ schedulers/schedule_sjf.c
@@ -1,5 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "task.h"
-#include "list.h"
+#include "../general/task.h"
+#include "../general/list.h"
+#include "../general/cpu.h"
+
+static node *head = NULL;
+
+int add(char *name, int priority, int burst){
+  static int tid = 0;
+  int check = 0;
+
+  Task *task_ptr = malloc(sizeof(Task));
+  if(task_ptr == NULL){
+    printf("Failure allocated task\n");
+    return -1;
+  }
+
+  task_ptr->name = strdup(name);
+  task_ptr->tid = tid;
+  task_ptr->priority = priority;
+  task_ptr->burst = burst;
+
+  if((check = insert(&head, task_ptr)) < 0){
+    printf("error adding task\n");
+    return -1;
+  }
+  traverse(head);
+  printf("\n");
+  return 0;
+}
+
+
+void schedule(){
+  int count = 0;
+  int slice;
+  int wait, response, turn_around;
+  node *temp;
+  Task *lowest;
+
+  while(head){
+    lowest = head->task;
+    temp = head->next;
+
+    while(temp){
+      if(temp->task->burst < lowest->burst){
+        lowest = temp->task;
+      }
+      temp = temp->next;
+    }
+    slice = lowest->burst;
+
+    wait = count;
+    response = count;
+
+    run(lowest,lowest->burst);
+    delete(&head, lowest);
+
+    count += slice;
+    turn_around = count;
+
+    printf("Waiting Time: %d\n", wait);
+    printf("Turnaround Time: %d\n", turn_around);
+    printf("Response Time: %d\n", response);
+  } 
+}
